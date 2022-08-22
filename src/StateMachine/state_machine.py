b3068b49d2ff8b25baa0016bb87bc2c94282986a
Michael Mattie <codermattie@runbox.com>
2022-08-22 15:05:49 -0700

    first draft of state machine that could actually work

--- src/StateMachine/state_machine.py
+++ src/StateMachine/state_machine.py
@@ -5,189 +5,164 @@
 import boto3
 
 from json import loads, dumps
-from copy import deepcopy
-from deepdiff import DeepDiff 
+from datetime import datetime
 
-from functools import cached_property
+from collections import namedtuple
 
-REGION = 'us-west-2'
+from functools import cached_property, wraps
 
-CAPACITY = 0
+from config import REGION, MACHINE_TABLE
 
-if CAPACITY == 0:
-    BILLING_MODE = 'PAY_PER_REQUEST'
-else:
-    BILLING_MODE = 'PROVISIONED'
+StateMachineEvent = namedtuple('StateMachineEvent', ['event', 'data'])
 
-class StateMachine:
-    region = REGION
-
-    @property
-    def credentials(self):
-        sts_client = boto3.client('sts')
-
-        assume = sts_client.assume_role(
-            RoleArn=self.role,
-            RoleSessionName="StateMachineSession"
-        )
-
-        return assume['Credentials']
-
-    @cached_property
-    def dynamo(self):
-        creds = self.credentials
-            
-        return boto3.resource('dynamodb',
-            aws_access_key_id=creds['AccessKeyId'],
-            aws_secret_access_key=creds['SecretAccessKey'],
-            aws_session_token=creds['SessionToken']
-        )
-
-    def create_machine_table(self, environment, machine, **kwargs):
-        definition = {
-            'AttributeDefinitions': [
-                {
-                    'AttributeName': 'instance',
-                    'AttributeType': 'S'
-                },
-                {
-                    'AttributeName': 'timestamp',
-                    'AttributeType': 'S'    
-                },
-                {
-                    'AttributeName': 'state',
-                    'AttributeType': 'S'
-                }
-            ],
-            'TableName': machine + "_" + environment,
-            'KeySchema': [
-                {
-                    'AttributeName': 'instance',
-                    'KeyType': 'HASH'
-                }
-            ],
-            'BillingMode': BILLING_MODE
-        }
-
-    if BILLING_MODE == 'PROVISIONED':
-        definition['ProvisionedThroughput'] = {
-            'ReadCapacityUnits': CAPACITY,
-            'WriteCapacityUnits': CAPACITY,
-        }
-
-    if kwargs:
-        definition['Tags'] = []
-
-        for tag_key, tag_value in kwargs:
-            definition['Tags'].append({
-                'Key': tag_key,
-                'Value': tag_value,
-            })
-
-
-    def __init__(self, machine, profile, region=REGION):
-        session_default = {
-            'profile_name': profile
-        }
-            
-        self.region = region
-
-        boto3.setup_default_session(**session_default)
-
-
-
-
-        if dynamodb_resource is None:
-
-            machine_table=dynamodb_resource.Table(TABLE)
-
-        return machine_table
-
-        response = dynamodb_client.create_table(
+StateMachineHalt = namedtuple('StateMachineHalt', ['state', 'data', 'value'])
 
 class MachineStorageError(Exception):
-    def __init__(self, machine, system):
-        super().__init__("machine: %s" % machine, "system: %s" % system)
+    def __init__(self, machine_name, machine_instance, error):
+        super().__init__(self, f'Machine Storage Error: [%s,%s] -> %s', (machine_name,
+                                                                         machine_instance,
+                                                                         error))
+
+def state(state, other_states=[]):
+
+    def machine_generator(handler):
+
+        @wraps(handler)
+        def machine(self, state, **kwargs):
+            value, state, data = handler(self, state, **kwargs)
+
+            if state in other_states:
+                next = getattr(self, state)
+
+                if next:
+                    self.__dict__ = data
+                    self.state = state 
+
+                    next_value, next_state, next_data = next(self, state, **kwargs)
+
+                    return next_value, next_state, next_data
+
+                else:
+                    raise ValueError('StateMachine -> Unknown state %s' % state)
+            else:
+                raise ValueError('StateMachine -> handler %s cannot handle state %s' % (str(handler), state))
+    
+        return machine
+
+    return machine_generator
 
 
 class StateMachine:
-    machines = None
-    lazy = False
-
-    machine_key = None
-    machine_system = None
-
-    state = None
-
-    loaded = None
-
-    def fetch_or_create(self, state):
-        fetched = self.machines.get_item(Key={primaryKeyName: self.machine_key,
-                                              sortKeyName: self.machine_system})
-        if not fetched:
-            new_data = {'machine': self.machine_key,
-                       'system': self.machine_system,
-                       'state': state,
-                       'data': {state: {}}}
-
-            response = self.machines.put_item(Item=new_data)
-
-            if not response:
-                raise MachineStorageError(self.machine_key, self.machine_system)
-
-            self.loaded = new_data['data'][state]
-
-        else:
-            fetched = json.loads(fetched)
-            self.loaded = fetched['data'][state]
-
-        return self.loaded
-
-
-    def persist(self, Force=False):
-        dirty = False
-        if DeepDiff(self.loaded, self.__dict__):
-            dirty = True
-
-        if Force or (self.lazy is False and dirty is True):
-            updated_data = self.loaded
-            updated_data.update(self.__dict__)
-
-            response = self.machines.update_item(Key={'machine': self.machine_key},
-                                                 UpdateExpression="SET #state = :state, #data[\"%\"] = :data" % self.state,
-                                                 ExpressionAttributeValues={':state': self.state,
-                                                                            ':data': updated_data},
-                                                 ReturnValues='UPDATED_NEW')
-
-            if not response and updated_data:
-                raise MachineStorageError(self.machine_key, self.machine_system)
-
-           self.loaded[self.state] = updated_data
-
-
-    def flush(self):
-        self.persist(Force=True)
-
-    def __init__(self, state, machine, system, lazy=False):
-        self.machines = dynamo_machines()
-        self.lazy = lazy
-
+    def store_state(self, state, data, timestamp):
+        self.__dict__ = data
         self.state = state
+        self.timestamp = timestamp     
 
-        self.machine_key = machine
-        self.machine_group = system
+        return state, data
 
-        self.loaded = self.fetch_or_create(state)
+    def __init__(self, machine_name, machine_instance, initial_state, credentials={}):
+        self.credentials = credentials
+        self.machine_name = machine_name
+        self.machine_instance = machine_instance
 
-        self.__dict__ = copy.deepcopy(self.loaded)
+        self.state = initial_state
 
+        self.timestamp = None
 
-    def __assign__(self, state):
-            self.persist()
-            loaded = self.fetch_or_create(state)
+        self.pull_from_database()
 
+    def __get_item__(self, event):
+        try:
+            self.__dict__ = self.pull_from_database()
+
+            handler = getattr(self, self.state)
+ 
+            if handler:
+                value, state, data = handler(self, event.event, **event.data)
+
+                self.__dict__ = data
+                self.state = state
+
+                self.write_to_database(self.__dict__)
+
+                return value
+            else:
+                raise Exception("StateMachine -> unknown state %s" % self.state)
+
+        except StateMachineHalt as halt:
+            value, state, data = halt.value, halt.state, halt.data
+
+            self.__dict__ = data
             self.state = state
-            self.__dict__ = copy.deepcopy(loaded)
+
+            self.write_to_database(self.__dict__)
+
+            return value
+        
+    def __set_item__(self, event):
+        return self.__get_item(self, event)
+
+    @cached_property
+    def dynamo(self, **kwargs):            
+        return boto3.resource('dynamodb', **self.credentials)
+
+    @cached_property
+    def machine_table(self):
+        return self.dynamo.Table(MACHINE_TABLE)
+
+    def pull_from_database(self, initialize={}):
+        fetched = self.machine_table.get_item(Key={'primaryKeyName': self.machine_instance,
+                                                   'sortKeyName': self.machine_name})
+
+        if fetched:
+            fetched = loads(fetched)
+
+            database_data = fetched['data']
+            database_state = fetched['state']
+
+            database_timestamp = datetime.fromisoformat(fetched['timestamp'])
+
+            if self.timestamp and database_timestamp > self.timestamp:
+                self.store_state(database_data['state'], 
+                                 database_data, 
+                                 database_timestamp)
+
+            return database_state, database_data
+        else:
+            self.write_to_database(try_load=False)
+            
+            return self.state, self.__dict__
+
+    def write_to_database(self, try_load=True):
+        if try_load:
+            try_state, try_data = self.pull_from_database()
+
+            if try_state:
+                return try_state, try_data
+
+        new_timestamp = datetime.now()
+
+        entry = {'instance': self.machine_instance,
+                 'machine': self.machine_name,
+                 'timestamp': new_timestamp.isoformat(),
+                 'state': self.state,
+                 'data': dumps(self.__dict__)}
+
+        try:
+            response = self.machines.put_item(Item=entry)
+
+            if response:
+                self.timestamp = new_timestamp
+                return self.state, self.__dict__
+            else:
+                raise MachineStorageError(self.machine_name,
+                                          self.machine_instance,
+                                          f'No response writing dynamo table.')
+        except Exception as error:
+            raise MachineStorageError(self.machine_name,
+                                      self.machine_instance,
+                                      "Exception %s writing dynamo table." % str(error))
+
 
 
 
