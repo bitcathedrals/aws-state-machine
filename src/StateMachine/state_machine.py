40425f575a046ac66ab0099fbab11b2ee56e73c4
Michael Mattie <michael.mattie.employers@gmail.com>
2022-08-22 20:04:30 -0700

    another draft, this time correctly implementing the storage and handling of multiple states in the StateMachine class

--- src/StateMachine/state_machine.py
+++ src/StateMachine/state_machine.py
@@ -8,8 +8,11 @@ from json import loads, dumps
 from datetime import datetime
 
 from collections import namedtuple
+from copy import deepcopy
 
 from functools import cached_property, wraps
+
+from deepdiff import DeepDiff
 
 from config import REGION, MACHINE_TABLE
 
@@ -53,21 +56,63 @@ def state(state, other_states=[]):
 
 
 class StateMachine:
-    def store_state(self, state, data, timestamp):
-        self.__dict__ = data
-        self.state = state
-        self.timestamp = timestamp     
+    def update_current_state(self):
+        current_machines = self._machines
+        current_object = deepcopy(self.__dict__)
 
-        return state, data
+        del current_object['_machines']
 
-    def __init__(self, machine_name, machine_instance, initial_state, credentials={}):
-        self.credentials = credentials
-        self.machine_name = machine_name
-        self.machine_instance = machine_instance
+        old_object = current_machines[self._state]
 
-        self.state = initial_state
+        if DeepDiff(current_object, old_object):
+            current_object['_timestamp'] = datetime.now()  
 
-        self.timestamp = None
+            current_machines[self._state] = current_object
+
+            self.__dict__ = current_object
+            self._machines = current_machines
+
+            return True
+
+        return False
+ 
+    def set_new_data(self, data, timestamp):
+        self.update_current_state()
+
+        new_state = data
+
+        new_state['_state'] = self._state
+        new_state['_timestamp'] = timestamp
+        
+        new_machines = self._machines
+        new_machines[self._state] = new_state
+
+        new_state['_machines'] = new_machines
+
+        self.__dict__ = new_state
+
+    def switch_new_state(self, new_state):
+        self.update_current_state()
+
+        new_data = self._machines[new_state]
+        old_machines = self._machines
+
+        new_data['_machines'] = old_machines
+
+        self.__dict__ = new_state
+
+    def __init__(self, machine_name, machine_instance, initial_state, data={}, credentials={}):
+        self._credentials = credentials
+        self._machine_name = machine_name
+        self._machine_instance = machine_instance
+
+        self._state = initial_state
+
+        self._timestamp = None
+
+        self._machines = {}
+        self._machines[initial_state] = data
+        self.__dict__.update(self._machines[initial_state])
 
         self.pull_from_database()
 
@@ -99,8 +144,8 @@ class StateMachine:
 
             return value
         
-    def __set_item__(self, event):
-        return self.__get_item(self, event)
+    def __set_item__(self, state):
+        self.switch_new_state(state)
 
     @cached_property
     def dynamo(self, **kwargs):            
@@ -111,27 +156,30 @@ class StateMachine:
         return self.dynamo.Table(MACHINE_TABLE)
 
     def pull_from_database(self, initialize={}):
-        fetched = self.machine_table.get_item(Key={'primaryKeyName': self.machine_instance,
-                                                   'sortKeyName': self.machine_name})
+        fetch = self.machine_table.get_item(TableName=MACHINE_TABLE,
+                                            Key={'instance': {'N': self.machine_instance},
+                                                 'state': {'S': self._state},
+                                                 'machine': {'S': self.machine_name}})
 
-        if fetched:
-            fetched = loads(fetched)
+        if fetch:
+            parsed = loads(fetch)
 
-            database_data = fetched['data']
-            database_state = fetched['state']
-
-            database_timestamp = datetime.fromisoformat(fetched['timestamp'])
-
-            if self.timestamp and database_timestamp > self.timestamp:
-                self.store_state(database_data['state'], 
-                                 database_data, 
-                                 database_timestamp)
-
-            return database_state, database_data
-        else:
-            self.write_to_database(try_load=False)
+            database_timestamp = datetime.fromisoformat(parsed['timestamp'])
             
-            return self.state, self.__dict__
+            database_data = parsed['data']
+            database_data['_timestamp'] = database_timestamp
+
+            if self._timestamp and database_timestamp > self._timestamp:
+                self.set_new_data(database_data, 
+                                  database_timestamp)
+
+            return self._state, database_data
+        else:
+            self.set_new_data(initialize, datetime.now())
+
+            written = self.write_to_database(try_load=False)
+            
+            return self._state, written
 
     def write_to_database(self, try_load=True):
         if try_load:
@@ -140,24 +188,30 @@ class StateMachine:
             if try_state:
                 return try_state, try_data
 
-        new_timestamp = datetime.now()
+        timestamp = datetime.now()
+
+        data = deepcopy(self.__dict__)
+        del data['_machines']
+        del data['_timestamp']
 
         entry = {'instance': self.machine_instance,
+                 'state': self._state,
                  'machine': self.machine_name,
-                 'timestamp': new_timestamp.isoformat(),
-                 'state': self.state,
-                 'data': dumps(self.__dict__)}
+                 'timestamp': timestamp.isoformat(),
+                 'data': dumps(data)}
 
         try:
             response = self.machines.put_item(Item=entry)
 
             if response:
-                self.timestamp = new_timestamp
-                return self.state, self.__dict__
+                self._timestamp = timestamp
+                
+                return self._state, data
             else:
                 raise MachineStorageError(self.machine_name,
                                           self.machine_instance,
                                           f'No response writing dynamo table.')
+        
         except Exception as error:
             raise MachineStorageError(self.machine_name,
                                       self.machine_instance,
