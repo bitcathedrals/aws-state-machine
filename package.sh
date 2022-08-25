dc384947ab796114b3b3e1e2fcf0517583a9a5f2
Michael Mattie <michael.mattie.employers@gmail.com>
2022-08-25 00:17:05 -0700

    sync up package.sh with the work from cf_config

--- package.sh
+++ package.sh
@@ -7,23 +7,47 @@ PUBLISH_USER=packages
 
 VIRTUAL_PREFIX="machine"
 
+REGION='us-west-2'
+VERSION=0.1.0
+
+function add_src {
+    site=`pyenv exec python -c 'import site; print(site.getsitepackages()[0])'`
+
+    echo "include_src: setting dev.pth in $site/dev.pth"
+
+    test -d $site || mkdir -p $site
+    echo "$PWD/src/" >"$site/dev.pth"
+    echo "$PWD/scripts/" >>"$site/dev.pth"
+}
+
+function remove_src {
+    site=`pyenv exec python -c 'import site; print(site.getsitepackages()[0])'`
+
+    echo "include_src: setting dev.pth in $site/dev.pth"
+
+    test -d $site || mkdir -p $site
+    echo "$PWD/src/" >"$site/dev.pth"
+    echo "$PWD/scripts/" >>"$site/dev.pth"
+}
+
 case $1 in
 
 #
 # tooling
 #
-
     "install-tools")
         brew update
 
         brew install pyenv
         brew install pyenv-virtualenv
+        brew install git-flow
     ;;
     "update-tools")
         brew update
 
         brew upgrade pyenv
         brew upgrade pyenv-virtualenv
+        brew upgrade git-flow
     ;;
 
 #
@@ -57,36 +81,56 @@ case $1 in
         pipenv graph
     ;;
     "test")
-        PYTHONPATH="$PYTHONPATH:src" pyenv exec python -m pytest tests
+        pyenv exec python -m pytest tests
+    ;;
+    "paths")
+        shift
+        add_src
+        pyenv exec python -c "import sys; print(sys.path)"
     ;;
     "python")
         shift
-        PYTHONPATH="$PYTHONPATH:src" pyenv exec python $@
+        pyenv exec python $@
     ;;
     "run")
         shift
-        PYTHONPATH="$PYTHONPATH:src" pyenv exec $@ 
+        pyenv exec $@ 
     ;;
     "aws")
         shift
         pyenv exec python -m awscli $@
     ;;
+    "cf")
+        shift
 
+        export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+                    $(aws sts assume-role \
+                    --role-arn arn:aws:iam::324189914596:role/devCFconfigBuildRole \
+                    --role-session-name DevCloudFormationSession \
+                    --profile dev \
+                    --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+                    --output text))
+
+        pyenv exec python -m awscli --region $REGION $@
+    ;;
     "validate")
         pyenv exec python -m awscli cloudformation validate-template --template-body file://$2 --profile $AWS_PROFILE
    ;;
 
-    "cli-delete")
+    "delete")
         echo "attempting to delete stack: $2"
         PYTHONPATH="$PYTHONPATH:src" pyenv exec python -m awscli cloudformation delete-stack --stack-name $2
     ;;
-    "cli-describe")
+    "describe")
         echo "attempting to describe stack: $2"
         PYTHONPATH="$PYTHONPATH:src" pyenv exec python -m awscli cloudformation describe-stacks --stack-name $2
     ;;
 
     "build")
         pyenv exec python -m build
+
+        find . -name '*.egg-info' -type d -print | xargs rm -r 
+        find . -name '__pycache__' -type d -print | xargs rm -r 
     ;;
 
 #
@@ -102,27 +146,53 @@ case $1 in
         pyenv exec python -m pipenv install --dev --skip-lock
         pyenv rehash
     ;;
-    "push")
-        test -d ~/coding/python-packages/simple/StateMachine || mkdir -p ~/coding/python-packages/simple/StateMachine
-        cp dist/* ~/coding/python-packages/simple/StateMachine/
-    ;;
     "list")
         pyenv exec python -m pipenv graph
     ;;
 #
 # release environment
 #
-    "freeze")
-        pyenv exec python -m pipenv lock
-    ;;
-    "requirements")
-        pyenv exec python -m pipenv run python -m pip freeze
-    ;;
-    "release")
+   "release-start")
         pyenv exec python -m pyenv -m pip -U pip
         pyenv exec python -m pyenv install -U pipenv
         pyenv exec python -m pipenv install --ignore-pipfile
         pyenv rehash
+
+        test -d releases || mkdir releases
+        pyenv exec python -m pipenv lock
+
+        mv Pipfile.lock releases/Pipfile.lock-$VERSION
+        cp Pipfile releases/Pipfile-$VERSION
+    ;;
+    "release-finish")
+        git push --all
+        git push --tags
+    ;;
+    "deploy-m1")
+        pyenv exec python -m build
+
+        DIST_PATH="/Users/michaelmattie/coding/python-packages/"
+        PKG_PATH="$DIST_PATH/simple/cfconfig"
+        BEAST="michaelmattie@beast.local"
+
+        ssh $BEAST "test -d $PKG_PATH || mkdir $PKG_PATH"
+        scp dist/* "$BEAST:$PKG_PATH/"
+        ssh $BEAST "cd $DISTPATH && /bin/bash upload-new-packages.sh"
+        ssh $BEAST "cd $DISTPATH && mv simple/cfconfig/* remote/cfconfig/ && /bin/bash update-packages.sh"
+    ;;
+    "deploy-intel")
+        pyenv exec python -m build
+
+        find . -name '*.egg-info' -type d -print | xargs rm -r 
+        find . -name '__pycache__' -type d -print | xargs rm -r 
+
+        DIST_PATH="/Users/michaelmattie/coding/python-packages/"
+        PKG_PATH="$DIST_PATH/simple/cfconfig"
+
+        test -d $PKG_PATH || mkdir $PKG_PATH
+        scp dist/* $PKG_PATH/
+        (cd $DISTPATH && /bin/bash upload-new-packages.sh)
+        (cd $DISTPATH && mv simple/cfconfig/* remote/cfconfig/ && /bin/bash update-packages.sh)
     ;;
     *)
         echo "unknown command."
