e7ccc39bc8c22bf7826b49b2c485f4762cee3d91
Michael Mattie <codermattie@runbox.com>
2022-08-26 11:57:56 -0700

    update py.sh config

--- python.sh
+++ python.sh
@@ -1,4 +1,4 @@
-PACKAGE_PYTHON_VERSION="3.10:latest"
+PYTHON_VERSION="3.10:latest"
 
 VIRTUAL_PREFIX="machine"
 
@@ -7,3 +7,6 @@ VERSION=0.1.0
 
 AWS_PROFILE=dev
 AWS_ROLE=arn:aws:iam::324189914596:role/devCFconfigBuildRole
+
+EDITOR=nano
+
