7ee3d6b39df1161d58713dc48745bd01defe6f6f
Michael Mattie <michael.mattie.employers@gmail.com>
2022-08-25 20:27:37 -0700

    install pythonsh

---
+++ python.sh
@@ -0,0 +1,9 @@
+PACKAGE_PYTHON_VERSION="3.10:latest"
+
+VIRTUAL_PREFIX="machine"
+
+REGION='us-west-2'
+VERSION=0.1.0
+
+AWS_PROFILE=dev
+AWS_ROLE=arn:aws:iam::324189914596:role/devCFconfigBuildRole
